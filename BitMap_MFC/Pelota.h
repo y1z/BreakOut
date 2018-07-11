8a85325bc94d58d0994390203e5e95330e5d8590
y1z <32182964+y1z@users.noreply.github.com>
2018-07-10 22:22:34 -0500

    e l primer commit

---
+++ BitMap_MFC/Pelota.h
@@ -0,0 +1,12 @@
+#pragma once
+// Nota averiguar como crear un "class.h" aparte de los demas para no tener que usar struct.
+struct Pelota {
+
+public:// Variables 
+	int PosicionEnY = 0;
+	int PosicionEnX = 0;
+	int VelocidadEnY = 0;
+	int VelocidadEnX = 0;
+
+
+};
