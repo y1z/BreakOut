87891cccfd415b65ffa1154a7303e9b121551ffb
y1z <32182964+y1z@users.noreply.github.com>
2018-08-11 09:59:24 -0500

    11/agosto/2018
    Experimento con Resultados no desiados
    -----------------------------------------
    Intente pasar algos valores para el juegue dentro de una estructura , el problema es que por alguna razon
    cuando intento dibujar el bitmap de la pelota todo lo que me da es el color blanco en la misma forma .

--- BitMap_MFC/EstructraDatosJuego.h
+++ BitMap_MFC/EstructraDatosJuego.h
@@ -1,15 +1,19 @@
 #pragma once
 
 #include "stdafx.h"
+#include "afxdialogex.h"
 
 // todo lo que va hacer este es almacenar datos dentro de si mismo para no tener que  
 //Pasar por referencia todos los datos todo el timepo .
 struct SDatosJuego {
-	CDC *m_pDC = nullptr;
-	CDC m_bmPC;
-	
-	RECT m_miRect;
 
+	CBitmap *m_ptrBitMap;
+	CBitmap m_BitMap;
+public:
+	CDC *m_pDC;
+	CDC *m_bmDC;
+public:
+	RECT m_miRect;
 
 
 };
